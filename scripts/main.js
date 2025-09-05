eb475c37f98cb5d9a1f03e99c3233a322a432ceb
LS_WoodHouse <ls.woodhouse.verhovyna@gmail.com>
2025-09-05 13:31:14 +0300

    .

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,26 @@
 <script>
-  let currentIndex = 0;
+    let currentIndex = 0;
 
-  function showSlide(index) {
+    function showSlide(index) {
       const slider = document.getElementById('slider');
-  const totalSlides = slider.children.length;
+    const totalSlides = slider.children.length;
 
-  if (index < 0) {
-    currentIndex = totalSlides - 1;
+    if (index < 0) {
+        currentIndex = totalSlides - 1;
       } else if (index >= totalSlides) {
-    currentIndex = 0;
+        currentIndex = 0;
       } else {
-    currentIndex = index;
+        currentIndex = index;
       }
 
-  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
     }
 
-  function nextSlide() {
-    showSlide(currentIndex + 1);
+    function nextSlide() {
+        showSlide(currentIndex + 1);
     }
 
-  function prevSlide() {
-    showSlide(currentIndex - 1);
+    function prevSlide() {
+        showSlide(currentIndex - 1);
     }
 </script>
